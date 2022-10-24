f7d676abc59f066d8e46f6fcf3c3eaa3ec82ccdd
unknown <you@example.com>
2022-10-24 22:15:03 +0200

    második

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
     require 'header.php';
     require 'navbar.php';
 ?>
+<main>
+    <div class="card" style="width: 18rem;">
+        <form action="">
+            <label for="username">Felhasználónév</label>
+            <br>
+            <input type="text" id="username">
+            <br>
+            <label for="password">Jelszó:</label>
+            <br>
+            <input type="password" id="password">
+        </form>      
+    </div>
+</main>
