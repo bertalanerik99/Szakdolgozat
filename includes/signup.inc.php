75851974596f000dc54dc0fd9a030cda42ca852f
unknown <you@example.com>
2022-11-10 22:43:39 +0100

    2022.11.10

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -36,5 +36,43 @@
         elseif (!preg_match("/^[+0-9]*$/", $phone)) {
             header("Location: ../signup.php?error=invalidphonenumber&mail=".$username."&email=".$email);
             exit();
+        }else{
+            #ezt a részt majd meg kell néznem újramert nem teljesen tiszta,hogy hogyan működik. A lényeg hogy el akarjuk kerülni hogy beírjanak e mezőbe egy sql parancsot.
+            $sql = "SELECT username FROM users  WHERE username=?";
+            $stmt = mysqli_stmt_init($connect);
+            if(!mysqli_stmt_prepare($stmt, $sql)){
+                header("Location: ../signup.php?error=sqlerror");
+                exit();
+            }
+            else{
+                mysqli_stmt_bind_param($stmt, "s", $username );
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                $resultcheck = mysqli_stmt_num_rows($stmt);
+                if($resultcheck > 0){
+                    header("Location: ../signup.php?error=usertaken&mail=".$email);
+                    exit();
+                }else{
+                    $sql = "INSERT INTO users (username, email, pwd, tel) VALUES (?, ?, ?, ?)";
+                    $stmt = mysqli_stmt_init($connect);
+                    if(!mysqli_stmt_prepare($stmt, $sql)){
+                        header("Location: ../signup.php?error=sqlerror");
+                        exit();
+                    }else{
+                        $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
+                        mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $hashedpassword, $phone);
+                        mysqli_stmt_execute($stmt);
+                        header("Location: ../signup.php?signup=success");
+                        exit();
+                        
+                    }
+                }
+            }
         }
+        mysqli_stmt_close($stmt);
+        mysqli_close($connect);
+    }
+    else{
+        header("Location: ../signup.php");
+        exit();
     }
